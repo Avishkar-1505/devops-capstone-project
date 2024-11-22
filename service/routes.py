2cd296262d31fbd4d74b4da337fd4215076daaa9
Avishkar-1505 <avishkarrl23087@gmail.com>
2024-11-22 16:02:59 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,7 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -72,7 +73,7 @@ def list_accounts():
 
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
+
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -89,7 +90,7 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
@@ -130,7 +131,7 @@ def delete_accounts(account_id):
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
-    
+
     if account:
         account.delete()
 
